21451735b8d21ab3ef9d594b403c65b757d04f68
Enmanuel Ochoa <163493441+Ema-Ochoa-07@users.noreply.github.com>
2024-11-26 09:45:14 -0500

    Creación proyecto backend y conexión BD H2

---
+++ src/main/java/gestion_productos_clientes/GestionProductosClientesBackendApplication.java
@@ -0,0 +1,13 @@
+package gestion_productos_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProductosClientesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProductosClientesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestion_productos_clientes/GestionProductosClientesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion_productos_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProductosClientesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
