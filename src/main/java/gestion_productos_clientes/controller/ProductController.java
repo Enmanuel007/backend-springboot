d26f8c8f743b49873d5e08b0dc769958b0e75f1a
Enmanuel Ochoa <eochoa0007@gmail.com>
2024-11-26 17:23:53 -0500

    Fix Add PostMapping in method posr product and client

--- src/main/java/gestion_productos_clientes/controller/ProductController.java
+++ src/main/java/gestion_productos_clientes/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ public class ProductController {
 	
 	
 	//Método para crear un producto
+	@PostMapping("/products")
 	public Product crearProducto(@RequestBody Product product) {
 		return repository.save(product);
 	}
